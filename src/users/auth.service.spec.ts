ee8787ba6abe80241c59343e494a3a25f8f3bc22
manil77 <manil.maharjan07@gmail.com>
2025-01-12 21:29:12 +0545

    Refactoring unit tests

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -2,20 +2,35 @@ import { Test } from "@nestjs/testing";
 import { AuthService } from "./auth.service";
 import { UsersService } from "./users.service";
 import { User } from "./user.entity";
+import { UsersController } from "./users.controller";
 
-it('can create an instance of auth service', async () => {
-    const fakeUsersService: Partial<UsersService> = {
-        find: () => Promise.resolve([]),
-        create: (email: string, password: string) => Promise.resolve({ id: 1, email, password } as User)
-    };
 
-    const module = await Test.createTestingModule({
-        providers: [ //we tried to create DI container.
-            AuthService,
-            {
-                provide: UsersService,
-                useValue: fakeUsersService
-            },
-        ],
-    }).compile();
-})
+//The describe block create an scope. It adds further description inside the tests written inside it
+describe('AuthService', () => {
+    let service: AuthService;
+
+    beforeEach(async () => {
+        const fakeUsersService: Partial<UsersService> = {
+            find: () => Promise.resolve([]),
+            create: (email: string, password: string) => Promise.resolve({ id: 1, email, password } as User)
+        };
+        const module = await Test.createTestingModule({
+            providers: [ //we tried to create DI container.
+                AuthService,
+                {
+                    provide: UsersService,
+                    useValue: fakeUsersService
+                },
+            ],
+        }).compile();
+
+        service = module.get(AuthService);
+    });
+
+
+    //Referencing 'service' variable safely inside all of our different tests
+    it('can create an instance of auth service', async () => {
+        expect(service).toBeDefined();
+    })
+
+});
