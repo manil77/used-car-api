cb4be5b2989409c3f9cf2c5e844d99568c7d7faf
manil77 <manil.maharjan07@gmail.com>
2025-01-18 08:59:56 +0545

    Added new test case, Changing mock implementation

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -3,14 +3,16 @@ import { AuthService } from "./auth.service";
 import { UsersService } from "./users.service";
 import { User } from "./user.entity";
 import { UsersController } from "./users.controller";
+import { BadRequestException } from '@nestjs/common';
+
 
 
 //The describe block create an scope. It adds further description inside the tests written inside it
 describe('AuthService', () => {
     let service: AuthService;
-
+    let fakeUsersService: Partial<UsersService>;
     beforeEach(async () => {
-        const fakeUsersService: Partial<UsersService> = {
+        fakeUsersService = {
             find: () => Promise.resolve([]),
             create: (email: string, password: string) => Promise.resolve({ id: 1, email, password } as User)
         };
@@ -42,5 +44,14 @@ describe('AuthService', () => {
         expect(salt).toBeDefined();
         expect(hash).toBeDefined();
     })
+
+    //Check if the email is in use while signing up
+    it('throws an error if user signs up with email that is in use', async () => {
+        fakeUsersService.find = () =>
+            Promise.resolve([{ id: 1, email: 'a', password: '1' } as User]);
+        await expect(service.signup('asdf@asdf.com', 'asdf')).rejects.toThrow(
+            BadRequestException,
+        );
+    });
 
 });
