bbe97f1c65c2bb8ae08db08d0cd1110241acbda3
Manil Maharjan <104967511+manil77@users.noreply.github.com>
2025-01-20 21:22:40 +0545

    .

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,77 +1,81 @@
 import {
-    NotFoundException, Session, Controller, Body, Get, Post, Param,
-    Patch, Delete, Query, UseGuards
-} from '@nestjs/common';
-import { CreateUserDto } from './dtos/create-user.dto';
-import { UsersService } from './users.service';
-import { User } from './user.entity';
-import { UpdateUserDto } from './dtos/update-user-dto';
-import { Serialize } from '../interceptors/serialize.interceptor';
-import { UserDto } from './dtos/user.dto';
-import { AuthService } from './auth.service';
-import { CurrentUser } from './decorators/current-user.decorator';
-import { AuthGuard } from '../guards/auth.guard';
-
-@Serialize(UserDto)
-@Controller('auth')
-export class UsersController {
-    constructor(private userService: UsersService, private authService: AuthService) { }
-
-    // @Get('/whoami')
-    // whoAmI(@Session() session: any) {
-    //     const user = this.userService.findOne(session.userId);
-    //     if (!user) {
-    //         throw new BadRequestException('User not found');
-    //     }
-    //     return user;
-    // }
-
+    Body,
+    Controller,
+    Post,
+    Get,
+    Patch,
+    Delete,
+    Param,
+    Query,
+    NotFoundException,
+    Session,
+    UseGuards,
+  } from '@nestjs/common';
+  import { CreateUserDto } from './dtos/create-user.dto';
+  import { UpdateUserDto } from './dtos/update-user.dto';
+  import { UsersService } from './users.service';
+  import { Serialize } from '../interceptors/serialize.interceptor';
+  import { UserDto } from './dtos/user.dto';
+  import { AuthService } from './auth.service';
+  import { CurrentUser } from './decorators/current-user.decorator';
+  import { User } from './user.entity';
+  import { AuthGuard } from '../guards/auth.guard';
+  
+  @Controller('auth')
+  @Serialize(UserDto)
+  export class UsersController {
+    constructor(
+      private usersService: UsersService,
+      private authService: AuthService,
+    ) {}
+  
     @Get('/whoami')
     @UseGuards(AuthGuard)
     whoAmI(@CurrentUser() user: User) {
-        return user;
+      return user;
     }
-
-    @Post('/signup')
-    async createUser(@Body() body: CreateUserDto, @Session() session: any) {
-        const user = await this.authService.signup(body.email, body.password);
-        session.userId = user.id
-        return user;
-    }
-
-    @Post('/signin')
-    async signIn(@Body() body: CreateUserDto, @Session() session: any) {
-        const user = await this.authService.signin(body.email, body.password)
-        session.userId = user.id;
-        return user;
-    }
-
-    @Get('/:id')
-    async findUser(@Param('id') id: string) {
-        const user = await this.userService.findOne(parseInt(id));
-        if (!user) {
-            throw new NotFoundException('User not found');
-        }
-        return user;
-    }
-
-    @Get()
-    findAll(@Query('email') email: string) {
-        return this.userService.find(email);
-    }
-
-    @Patch('/:id')
-    updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
-        return this.userService.update(parseInt(id), body);
-    }
-
-    @Delete('/:id')
-    deleteUser(@Param('id') id: string) {
-        return this.userService.remove(parseInt(id));
-    }
-
+  
     @Post('/signout')
     signOut(@Session() session: any) {
-        session.userId = null;
+      session.userId = null;
     }
-}
+  
+    @Post('/signup')
+    async createUser(@Body() body: CreateUserDto, @Session() session: any) {
+      const user = await this.authService.signup(body.email, body.password);
+      session.userId = user.id;
+      return user;
+    }
+  
+    @Post('/signin')
+    async signin(@Body() body: CreateUserDto, @Session() session: any) {
+      const user = await this.authService.signin(body.email, body.password);
+      session.userId = user.id;
+      return user;
+    }
+  
+    @Get('/:id')
+    async findUser(@Param('id') id: string) {
+      const user = await this.usersService.findOne(parseInt(id));
+      if (!user) {
+        throw new NotFoundException('user not found');
+      }
+      return user;
+    }
+  
+    @Get()
+    findAllUsers(@Query('email') email: string) {
+      return this.usersService.find(email);
+    }
+  
+    @Delete('/:id')
+    removeUser(@Param('id') id: string) {
+      return this.usersService.remove(parseInt(id));
+    }
+  
+    @Patch('/:id')
+    updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+      return this.usersService.update(parseInt(id), body);
+    }
+  }
+  
