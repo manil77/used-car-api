f3f8b46af28482b44afd17a27ef8ae64386a6ae7
manil77 <manil.maharjan07@gmail.com>
2025-01-21 19:57:04 +0545

    Fixing user.service test cases

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,15 +1,31 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
 
 describe('UsersService', () => {
   let service: UsersService;
+  let fakeUserRepository: Partial<Repository<User>>;
 
   beforeEach(async () => {
+    fakeUserRepository = {
+      // Add mock methods as necessary, e.g.,
+      find: jest.fn().mockResolvedValue([]),
+      save: jest.fn().mockResolvedValue({ id: 1, name: 'Test User' }),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: 'UserRepository',
+          useValue: fakeUserRepository, // Mock implementation of UserRepository
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
+    //service = module.get(UsersService);
   });
 
   it('should be defined', () => {
