014ab82a5da08b8f50a57af866e0becf3a80a693
manil77 <manil.maharjan07@gmail.com>
2025-01-31 12:01:34 +0545

    Setup development and test environment

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,18 +7,42 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users/user.entity';
 import { Report } from './reports/report.entity';
 import { APP_PIPE } from '@nestjs/core';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 const cookieSession = require('cookie-session');
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    type: 'sqlite',
-    database: 'db.sqlite',
-    entities: [User, Report],
-    synchronize: true,//this would migrate to the db any changes automatically if we would make changes on any entities (only when the app next starts up or runs).
-    // make synchronize: true  only in a development enviroment. 
-    // In production make synchronize: false so dont have any unneccessary changes or mistakes in the db. 
-    // In production we would migrate to the db normally
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal:true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
+  //   TypeOrmModule.forRoot({
+  //   type: 'sqlite',
+  //   database: 'db.sqlite',
+  //   entities: [User, Report],
+  //   synchronize: true,//this would migrate to the db any changes automatically if we would make changes on any entities (only when the app next starts up or runs).
+  //   // make synchronize: true  only in a development enviroment. 
+  //   // In production make synchronize: false so dont have any unneccessary changes or mistakes in the db. 
+  //   // In production we would migrate to the db normally
+  // })
+  //   TypeOrmModule.forRoot({
+  //   type: 'sqlite',
+  //   database: 'db.sqlite',
+  //   entities: [User, Report],
+  //   synchronize: true,
+  // }),
+  TypeOrmModule.forRootAsync({
+    inject:[ConfigService],
+    useFactory: (config: ConfigService) =>{
+      return {
+        type: 'sqlite',
+        database: config.get<string>('DB_NAME'),
+        synchronize:true,
+        entities:[User, Report],
+      }
+    }
   })
+
     , UsersModule
     , ReportsModule],
   controllers: [AppController],
