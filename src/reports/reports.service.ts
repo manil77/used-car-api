64a4f9bd7aa40fa8627bdf0c01ce9a835d2292a9
Manil Maharjan <manil.maharjan07@gmail.com>
2025-05-26 20:57:26 +0545

    Added query builder

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -4,12 +4,30 @@ import { Repository } from 'typeorm';
 import { Report } from './report.entity';
 import { CreateReportDto } from './dtos/create-report-dto';
 import { User } from 'src/users/user.entity';
+import { GetEstimateDto } from './dtos/get-estimate-dto';
 
 @Injectable()
 export class ReportsService {
   constructor(
     @InjectRepository(Report) private reportRepo: Repository<Report>,
   ) {}
+
+  createEstimate(estimateDto: GetEstimateDto) {
+    console.log(estimateDto);
+    return this.reportRepo
+      .createQueryBuilder()
+      .select('AVG(price)', 'price')
+      .where('make = :make', { make: estimateDto.make })
+      .andWhere('model = :model', { model: estimateDto.model })
+      .andWhere('lng - :lng BETWEEN -5 AND 5 ', { lng: estimateDto.lng })
+      .andWhere('lat - :lat BETWEEN -5 AND 5 ', { lat: estimateDto.lat })
+      .andWhere('year - :year BETWEEN -3 AND 3 ', { year: estimateDto.year })
+      .andWhere('approved IS TRUE')
+      .orderBy('mileage - :mileage', 'DESC')
+      .setParameters({ mileage: estimateDto.mileage })
+      .limit(3)
+      .getRawOne();
+  }
 
   create(reportDto: CreateReportDto, user: User) {
     const report = this.reportRepo.create(reportDto);
